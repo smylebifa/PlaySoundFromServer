fee888f3a6ebba2cb7d08887286e4732f4e37a64
smyle-pro <smaylic111new@yandex.ru>
2020-12-07 12:13:07 +0300

    add few clients to service

--- src/ru/samoilov/Client.java
+++ src/ru/samoilov/Client.java
@@ -9,7 +9,9 @@ import java.util.zip.ZipInputStream;
 
 public class Client {
 
-  public static void main(String[] args) throws Exception {
+  static volatile boolean exit = false;
+
+  public static void main(String[] args) throws IOException {
 
     // Создаем сокет используя IP-адрес и порт сервера...
     Socket socket = new Socket("localhost", 3000);
@@ -24,74 +26,94 @@ public class Client {
       // Создаем поток для записи сжатого звукового файла.
       // Создаем потоки для распаковки и записи в файл...
       InputStream inputStream = socket.getInputStream();
-      FileOutputStream fileOutputStream = new FileOutputStream(pathMusicFileZip);
-      ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(pathMusicFileZip));
-      FileOutputStream fileOutputStreamFile = new FileOutputStream(pathMusicFile);
       DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+      File file = new File(pathMusicFile);
+
+      // Создаем поток для чтения с клавиатуры...
+      BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
       while (true) {
-        // Создаем поток для чтения с клавиатуры...
-        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Enter Get to get music file or Close to close program.");
 
         String commandName = keyboard.readLine();
 
         if (commandName.equals("Close") || commandName.equals("close")) break;
 
-        System.out.println("Sending client command to the server...");
-        dataOutputStream.writeUTF(commandName); // Отсылаем введеную команду серверу...
-        dataOutputStream.flush(); // Заставляем поток закончить передачу данных...
-        
+        else if (commandName.equals("Get") || commandName.equals("get")) {
 
-        // Получение сжатого звукового файла с сервера...
-        System.out.println("Transferring compressed file");
+          ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(pathMusicFileZip));
+          FileOutputStream fileOutputStreamFile = new FileOutputStream(pathMusicFile);
 
-        int c = 0;
-        while ((c = inputStream.read()) >= 0) {
-          fileOutputStream.write((char) c);
+          System.out.println("Sending client command to the server...");
+          dataOutputStream.writeUTF(commandName); // Отсылаем введеную команду серверу...
+          dataOutputStream.flush(); // Заставляем поток закончить передачу данных...
+
+          // Получение сжатого звукового файла с сервера...
+          System.out.println("Transferring compressed file");
+
+          Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+              try {
+                FileOutputStream fileOutputStream = new FileOutputStream(pathMusicFileZip);
+                int c = 0;
+                while ((c = inputStream.read()) != -1 || exit) {
+                  fileOutputStream.write(c);
+                }
+                exit = false;
+                fileOutputStream.close();
+
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
+          });
+
+          thread.start();
+          Thread.sleep(15000);
+          exit = true;
+          System.out.println("Compressed file was transferred");
+
+
+          // Получаем сжатый файл для считывания...
+          zipInputStream.getNextEntry();
+
+          // Распаковываем архив и записываем в звуковой файл...
+          for (int c = zipInputStream.read(); c != -1; c = zipInputStream.read()) {
+            fileOutputStreamFile.write(c);
+          }
+
+          // Закрываем потоки...
+          fileOutputStreamFile.close();
+          zipInputStream.closeEntry();
+          zipInputStream.close();
+
+          System.out.println("Compressed file was saved");
+
+          // Получаем поток для воспроизведения записанного файла...
+          AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+
+          // Воспроизводим звук из выбранного файла...
+          System.out.println("Playing sound");
+
+          Clip clip = AudioSystem.getClip();
+          clip.open(audioInputStream);
+          clip.start();
+
+          // Ждем завершения воспроизведения аудиофайла...
+          Thread.sleep(3000);
+          clip.stop();
+          clip.close();
+
+          audioInputStream.close();
+
+          System.out.println();
         }
-
-        System.out.println("Compressed file was transferred");
-
-        // Закрываем поток...
-        fileOutputStream.close();
-
-
-        // Получаем сжатый файл для считывания...
-        zipInputStream.getNextEntry();
-
-        // Распаковываем архив и записываем в звуковой файл...
-        for (c = zipInputStream.read(); c != -1; c = zipInputStream.read()) {
-          fileOutputStreamFile.write(c);
-        }
-
-        // Закрываем потоки...
-        fileOutputStreamFile.close();
-        zipInputStream.closeEntry();
-        zipInputStream.close();
-
-        System.out.println("Compressed file was saved");
-
-
-        // Получаем поток для воспроизведения записанного файла...
-        File file = new File(pathMusicFile);
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
-
-        // Воспроизводим звук из выбранного файла...
-        System.out.println("Playing sound");
-
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioInputStream);
-        clip.start();
-
-        // Ждем завершения воспроизведения аудиофайла...
-        Thread.sleep(3000);
-        clip.stop();
-        clip.close();
-
-        System.out.println();
       }
-    } finally {
+    } catch (Exception e) {e.printStackTrace();}
+
+    finally {
       System.out.println("closing...");
       socket.close();
     }
--- src/ru/samoilov/Server.java
+++ src/ru/samoilov/Server.java
@@ -6,49 +6,63 @@ import java.net.Socket;
 
 public class Server {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws IOException {
 
     // Создаем сокет сервера и привязываем его к вышеуказанному порту...
     ServerSocket serverSocket = new ServerSocket(3000);
     System.out.println("Waiting for a client...");
 
     try {
+      while (true) {
+        // Заставляем сервер ждать подключений и выводим сообщение когда кто-то связался с сервером...
+        Socket socket = serverSocket.accept();
+        System.out.println("Got a client.");
 
-      // Заставляем сервер ждать подключений и выводим сообщение когда кто-то связался с сервером...
-      Socket socket = serverSocket.accept();
-      System.out.println("Got a client.");
+        try {
 
-      try {
-        // Создаем входной поток для получения сжатого звукового файла
-        // и выходной поток сокета для отправки данных клиенту...
-        InputStream inputStream = new FileInputStream("ZipSound/ZipFile.zip");
-        OutputStream outputStream = socket.getOutputStream();
-        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+          Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+              try {
+                // Создаем входной поток для получения сжатого звукового файла
+                // и выходной поток сокета для отправки данных клиенту...
+                InputStream inputStream = new FileInputStream("ZipSound/ZipFile.zip");
+                OutputStream outputStream = socket.getOutputStream();
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
-        String commandName = dataInputStream.readUTF();
+                String commandName = dataInputStream.readUTF();
 
-        // Если введена команда верно, то получаем из архива и воспроизводим звуковой файл...
-        if (commandName.equals("Get") || commandName.equals("get")) {
+                // Если введена команда верно, то получаем из архива и воспроизводим звуковой файл...
+                if (commandName.equals("Get") || commandName.equals("get")) {
 
-          // Передаем архив со звуковым файлом клиенту...
-          System.out.println("Transferring compressed file");
+                  // Передаем архив со звуковым файлом клиенту...
+                  System.out.println("Transferring compressed file");
 
-          int c = 0;
-          while ((c = inputStream.read()) >= 0) {
-            outputStream.write(c);
-            outputStream.flush();
-          }
+                  int c = 0;
+                  while ((c = inputStream.read()) >= 0) {
+                    outputStream.write(c);
+                    outputStream.flush();
+                  }
 
-          System.out.println("File was transferred");
+                  System.out.println("File was transferred");
+                }
+              } catch (IOException e) {e.printStackTrace();}
+            }
+          });
+
+          thread.start();
+
+          thread.join();
+
+        } catch (Exception e) {e.printStackTrace();}
+
+        finally {
+          System.out.println("closing...");
+          socket.close();
         }
 
       }
-      finally {
-        System.out.println("closing...");
-        socket.close();
-      }
-    }
-    finally {
+    } finally {
       serverSocket.close();
     }
   }
